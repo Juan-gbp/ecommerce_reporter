ce3a48385d167c26208616d852be72363c0f6b3d
Juan <59241142+Juan-gbp@users.noreply.github.com>
2020-11-18 11:39:08 -0500

    Update dummy_collector.py
    add the web-scrapping feature

--- dummy_collector.py
+++ dummy_collector.py
@@ -1,4 +1,19 @@
 from redis import Redis
+#https://www.bjjhq.com/
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://www.bjjhq.com/')
+
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+#soup = soup.find('div', id="outerDiv").find('div', id="main").find('div', id="Panel1")
+#soup = soup.find('div', id="fullWidthWrapper").find('div', id="wrapper").find('div', id="container")
+name = soup.find('h1').text
+soup = soup.find('div', id="buyBox").find('div', id="buyButton")
+price = soup.find('em').text
+
 
 r = Redis(host='localhost', port=6379, db=0)
 
@@ -10,10 +25,16 @@ class MyClass:
   def set_price(self, price):
     self.price = price
     
+  def set_name(self, name):
+    self.name = name
+    
 
 iter1 = MyClass()
-iter1.set_price(8)
+iter1.set_price(price)
+iter1.set_name(name)
 
 r.set('Price', iter1.price)
+r.set('Name', iter1.name)
 
 print(r.get('Price'))
+print(r.get('Name'))
